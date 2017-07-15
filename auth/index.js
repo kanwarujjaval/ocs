5295f58ca2459884c53dd5899ed26ae68c6e1ca5
Kanwar Ujjaval Singh <ujjavalkanwar@gmail.com>
2017-07-15 11:57:44 +0530

    init auth class

---
+++ auth/index.js
@@ -0,0 +1,13 @@
+/** Main Auth class for Authentication */
+class Auth {
+
+    /**
+     * create a new Authentication Object
+     * @param {Object} config - Instance of Config class.
+     */
+    constructor(config){
+        this._config  = config;
+    }
+}
+
+module.exports = Auth;
